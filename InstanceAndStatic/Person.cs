ca19ab89bd59e0bffc9828de5bfbac1b256944ac
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-07 18:57:44 +0200

    instance and static members examples

---
+++ InstanceAndStatic/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace InstanceAndStatic
+{
+    internal class Person
+    {
+        public string FirstName = "John";
+        public string LastName = "Doe";
+
+        public static int PopulationCount = 0;
+    }
+}
---
+++ InstanceAndStatic/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InstanceAndStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person = new Person();
+            Console.WriteLine(person.FirstName); 
+            Console.WriteLine(person.LastName);
+
+            Console.WriteLine(Person.PopulationCount);
+        }
+    }
+}
